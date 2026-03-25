a47c8ab3411437e3e97499037a4ff6bfcc5ef49f
Michael Rdoriguez <maicolrdgz123@gmail.com>
2026-03-25 08:07:43 -0500

    password encrypted

--- src/singup.php
+++ src/singup.php
@@ -6,10 +6,11 @@
     $e_email = $_POST['email'];
     $m_phone = $_POST['mphone'];
     $p_sswd = $_POST['passwd'];
+    $enc_pass = md5($pswd);
 
     //query to insert into SQL
     $sql = "INSERT INTO users (firstname, lastname,
-    email, mobile_phone , password) VALUES('$f_name','$l_name','$e_email','$m_phone','$p_sswd')";
+    email, mobile_phone, password) VALUES('$f_name','$l_name','$e_email','$m_phone','$enc_pass')";
 
     //execute query
     pg_query($sql);
