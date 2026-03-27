839bbbda7b4bc45a315ca4e7224ff26a15356c38
Michael Rdoriguez <maicolrdgz123@gmail.com>
2026-03-27 12:43:08 -0500

    local & supa registration

--- src/singup.php
+++ src/singup.php
@@ -12,6 +12,19 @@
     $sql = "INSERT INTO users (firstname, lastname,
     email, mobile_phone, password) VALUES('$f_name','$l_name','$e_email','$m_phone','$enc_pass')";
 
+    //local & supa
+    if ($res_local) {
+    $res_supa = pg_query($supa_conn, $sql);
+
+    if ($res_supa) {
+        echo "Registro exitoso.";
+    } else {
+        echo "Error: el registro se guardo en local, no en la nube.";
+    }
+    } else {
+    echo "Error: el registro no se guardo en ningun sitio.";
+    }
+
     //execute query
     pg_query($sql);
     ?>
